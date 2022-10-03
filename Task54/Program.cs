fb2fe485a33dfd985fa7405a0b0f24518bd1b5e0
Marina Vystavkina <vistavkinamm@mail.ru>
2022-10-03 13:49:09 +0300

    Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int rows = new Random().Next(4,5);
+int columns = new Random().Next(4,5);
+int[,] matrix = new int[rows, columns];
+CreateArray (matrix);
+WriteArray (matrix);
+
+void OrderArrayLines(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if  (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void CreateArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(10);
+        }
+    }
+}
+
+void WriteArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
+Console.WriteLine("Упорядоченный массив: ");
+OrderArrayLines(matrix);
+WriteArray(matrix);
